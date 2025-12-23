2e4a66b9c45d7b6dedf8f7cea70ba34e04c36afa
Malay Dewangan <malaydewangan310@gmail.com>
2025-12-23 11:31:49 +0530

    feat(): introduce Discord plugin

--- src/main/java/io/kestra/plugin/discord/DiscordExecution.java
+++ src/main/java/io/kestra/plugin/discord/DiscordExecution.java
@@ -21,7 +21,7 @@ import java.util.Map;
 @Schema(
     title = "Send a Discord message with the execution information.",
     description = "The message will include a link to the execution page in the UI along with the execution ID, namespace, flow name, the start date, duration, and the final status of the execution. If failed, then the task that led to the failure is specified.\n\n" +
-        "Use this notification task only in a flow that has a [Flow trigger](https://kestra.io/docs/administrator-guide/monitoring#alerting). Don't use this notification task in `errors` tasks. Instead, for `errors` tasks, use the [DiscordIncomingWebhook](https://kestra.io/plugins/plugin-notifications/tasks/discord/io.kestra.plugin.discord.discordincomingwebhook) task."
+        "Use this notification task only in a flow that has a [Flow trigger](https://kestra.io/docs/administrator-guide/monitoring#alerting). Don't use this notification task in `errors` tasks. Instead, for `errors` tasks, use the [DiscordIncomingWebhook](https://kestra.io/plugins/plugin-discord/io.kestra.plugin.discord.discordincomingwebhook) task."
 )
 @Plugin(
     examples = {
