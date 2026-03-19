4c3f42f0f2289696c64d779ba3d20935e079d391
François Delbrayelle <fdelbrayelle@kestra.io>
2026-03-19 15:27:38 +0100

    fix: correct Property.ofValue/ofExpression type mismatches and rendering issues

--- src/test/java/io/kestra/plugin/discord/DiscordIncomingWebhookTest.java
+++ src/test/java/io/kestra/plugin/discord/DiscordIncomingWebhookTest.java
@@ -47,7 +47,7 @@ public class DiscordIncomingWebhookTest {
         DiscordIncomingWebhook task = DiscordIncomingWebhook.builder()
             .url(embeddedServer.getURI() + "/webhook-unit-test")
             .payload(
-                Property.ofValue(
+                Property.ofExpression(
                     Files.asCharSource(
                         new File(
                             Objects.requireNonNull(
